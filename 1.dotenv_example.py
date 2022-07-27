85cc7ab1f722741d2ebb8e61438cf8152d6be8aa
Gurpreet Singh <Gurpreets0147@gmail.com>
2022-07-27 17:38:47 +0530

    New Files

--- 1.dotenv_example.py
+++ 1.dotenv_example.py
@@ -1,12 +1,9 @@
-from dotenv import load_dotenv,dotenv_values
+from dotenv import load_dotenv, dotenv_values
 
-env_file=load_dotenv(".demo_env")  # take environment variables from .env.
-data=dotenv_values(".demo_env")
+env_file = load_dotenv("env.txt")  # take environment variables from .env.
+data = dotenv_values("env.txt")
 
 print(data)
-
-
-
 
 # Code of your application, which uses environment variables (e.g. from `os.environ` or
 # `os.getenv`) as if they came from the actual environment.

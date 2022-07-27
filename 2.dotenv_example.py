041d4331a62434124716b494d31c8e3f94eb4d44
Gurpreet Singh <Gurpreets0147@gmail.com>
2022-07-27 17:35:48 +0530

    New Files

--- 2.dotenv_example.py
+++ 2.dotenv_example.py
@@ -3,7 +3,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.demo_env')  #Path to the file
+dotenv_path = join(dirname(__file__), 'env.txt')  #Path to the file
 load_dotenv(dotenv_path)
 
 DOMAIN = os.environ["DOMAIN"]
